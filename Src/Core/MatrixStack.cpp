3f9f5c5ea61d9554933922633efb60584c954a97
Dan Watkins <dwatkins@valpineware.com>
2014-04-27 23:07:50 -0500

    Finished implementing MatrixStack

--- Src/Core/MatrixStack.cpp
+++ Src/Core/MatrixStack.cpp
@@ -19,4 +19,30 @@ namespace t3d
 		: mCurrentMatrix(initialMatrix)
 	{
 	}
+
+	void MatrixStack::perspective(Float fovDeg, Float aspectRatio, Float zNear, Float zFar)
+	{
+		mCurrentMatrix *= glm::perspective(fovDeg, aspectRatio, zNear, zFar);
+	}
+
+
+	void MatrixStack::orthographic(Float left, Float right, Float bottom, Float top, Float zNear = 1.0f, Float zFar = 1.0f)
+	{
+		mCurrentMatrix *=  glm::ortho(left, right, bottom, top, zNear, zFar);
+	}
+
+
+	void MatrixStack::pixelPerfectOrtho(glm::ivec2 size, Vec2f depthRange, Bool isTopLeft = true)
+	{
+		if (isTopLeft)
+		{
+			translate(-1.0f, 1.0f, (depthRange.x + depthRange.y) / 2.0f);
+			scale(2.0f / size.x, -2.0f / size.y, 1.0f);
+		}
+		else
+		{
+			translate(-1.0f, -1.0f, (depthRange.x + depthRange.y) / 2.0f);
+			scale(2.0f / size.x, 2.0f / size.y, 2.0f / (depthRange.y - depthRange.x));
+		}
+	}
 };
