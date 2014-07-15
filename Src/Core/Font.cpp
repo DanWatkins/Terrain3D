4e31dbb1921adfaad1b9c0b96b7260dfb909bb55
Dan Watkins <dwatkins@valpineware.com>
2014-07-14 19:36:48 -0500

    Began implementation of Font class (based on ge::Font from GlanceEngineSDK)

---
+++ Src/Core/Font.cpp
@@ -0,0 +1,41 @@
+//==================================================================================================================|
+// Created 2014.06.14 by Daniel L. Watkins
+//
+// Copyright (C) 2014 Daniel L. Watkins
+// This file is licensed under the MIT License.
+//==================================================================================================================|
+
+#include "Font.h"
+
+namespace t3d
+{
+	Font::Font()
+	{
+		memset(&mWidths, 0, sizeof(mWidths));
+	}
+
+
+	bool Font::load(const String &fontname, const String &containingDirectoryPath)
+	{
+		unsigned char buffer[512];
+
+		std::ifstream file;
+		file.open((containingDirectoryPath + "/" + fontname + ".dat").c_str(), std::ios::binary);
+
+		if (!file.is_open())
+			return false;
+
+		//convert from raw data to width data
+		for (int n=0; n<256; n++)
+			mWidths[n] = (int)buffer[n*2];
+
+		//load the image
+		mImage.loadFromFile_PNG((containingDirectoryPath + "/" + fontname + ".png"));
+		mSpriteSheet.init(mImage, 16, 16);
+	}
+
+
+	void Font::print(const OpenGLWindow &window, const String &text, const Vec2f &pos) const
+	{
+	}
+};
---
+++ Src/Core/Font.h
@@ -0,0 +1,38 @@
+//==================================================================================================================|
+// Created 2014.06.14 by Daniel L. Watkins
+//
+// Copyright (C) 2014 Daniel L. Watkins
+// This file is licensed under the MIT License.
+//==================================================================================================================|
+
+#ifndef _t3d_FONT_H
+#define _t3d_FONT_H
+
+#include "../Main.h"
+#include "SpriteSheet.h"
+
+namespace t3d
+{
+	/**
+	 *
+	 */
+	class Font
+	{
+	private:
+		Image mImage;
+		SpriteSheet mSpriteSheet;
+		int mWidths[256];
+		int mVerticalOffset;
+
+	public:
+		Font();
+		~Font();
+
+		bool load(const String &fontname, const String &containingDirectoryPath);
+		void print(const OpenGLWindow &window, const String &text, const Vec2f &pos) const;
+	};
+};
+
+
+#endif
+
