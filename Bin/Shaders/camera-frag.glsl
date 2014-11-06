34b27898952977959d8071bfb075e51f2972820d
Dan Watkins <dwatkins@valpineware.com>
2014-11-06 13:30:25 -0600

    Abstracted texel access in the Camera FS

--- Bin/Shaders/camera-frag.glsl
+++ Bin/Shaders/camera-frag.glsl
@@ -10,21 +10,30 @@ layout (binding = 1) uniform sampler2DArray sampler;
 uniform float spacing;
 uniform int heightMapSize;
 
-void main()
+
+uint textureIndexForPos(vec4 pos)
+{
+	ivec2 indexPos;
+	indexPos.x = int(pos.x / spacing);
+	indexPos.y = int(pos.z / spacing);
+
+	int centerIndex = indexPos.x + indexPos.y*heightMapSize;
+	return texelFetch(buffer, centerIndex).r;
+}
+
+
+vec4 texelForIndex(uint index)
 {
 	vec3 pos;
 	pos.x = tc.x;
 	pos.y = tc.y;
+	pos.z = index;
 
-	//determine the terrain layer
-	{
-		ivec2 indexPos;
-		indexPos.x = int(outPos.x / spacing);
-		indexPos.y = int(outPos.z / spacing);
+	return texture(sampler, pos);
+}
 
-		int index = indexPos.x + indexPos.y*heightMapSize;
-		pos.z = texelFetch(buffer, index).r;
-	}
 
-	color = texture(sampler, pos);
+void main()
+{
+	color = texelForIndex(textureIndexForPos(outPos));
 }
