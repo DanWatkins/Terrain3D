5d0cdd0cddb00f31f667c23e2db548d52e5e642f
Dan Watkins <dwatkins@valpineware.com>
2014-12-11 23:09:36 -0600

    Added default value system that asserts if a default value is not defined for every key in the Key enum

--- Src/Application/Settings.cpp
+++ Src/Application/Settings.cpp
@@ -12,11 +12,15 @@
 
 QSettings *Settings::mSettings = nullptr;
 const QString Settings::mVersion = "0.0.0";
+QHash<Settings::Key, QVariant> Settings::mDefaultValues;
 
 //TODO man, you need to unit test this
 
 void Settings::init()
 {
+	initDefaultValues();
+	checkForMissingDefaultValues();
+
 	QString filepath = QDir::currentPath()+"/Terrain3D.ini";
 	mSettings = new QSettings(filepath, QSettings::IniFormat);
 	mSettings->setValue("Version", mVersion);
@@ -33,7 +37,7 @@ void Settings::setValue(Key key, const QVariant &value)
 
 QVariant Settings::value(Key key)
 {
-	return mSettings->value(stringNameForKey(key), "");
+	return mSettings->value(stringNameForKey(key), mDefaultValues[key]);
 }
 
 
@@ -42,4 +46,30 @@ QString Settings::stringNameForKey(Key key)
 	const QMetaObject &mo = Settings::staticMetaObject;
 	QMetaEnum me = mo.enumerator(mo.indexOfEnumerator("Key"));
 	return me.valueToKey(key);
+}
+
+
+void Settings::initDefaultValues()
+{
+	mDefaultValues[KeyShowHud] = true;
+}
+
+
+void Settings::checkForMissingDefaultValues()
+{
+	const QMetaObject &mo = Settings::staticMetaObject;
+	QMetaEnum me = mo.enumerator(mo.indexOfEnumerator("Key"));
+
+	for (int i=0; i<me.keyCount(); i++)
+	{
+		Key key = static_cast<Key>(me.value(i));
+
+		if (!mDefaultValues.contains(key))
+		{
+			QString msg = (QString("Settings: No default value defined for key ")
+								  + me.valueToKey(key));
+
+			qFatal(msg.toStdString().c_str());
+		}
+	}
 }
--- Src/Application/Settings.h
+++ Src/Application/Settings.h
@@ -9,6 +9,8 @@
 #define _t3d_SETTINGS_H
 
 #include <QtCore/QSettings>
+#include <QtCore/QHash>
+
 #include "Main.h"
 
 class Settings : public QObject
@@ -16,14 +18,14 @@ class Settings : public QObject
 	Q_OBJECT
 
 public:
-	Settings() {}
+	Settings() {}	//this is only here so QML can use it
 	static void init();
 
 	enum Key
 	{
-		KeyShowHud
+		KeyShowHud,
+		KeyFullscreen
 	}; Q_ENUMS(Key)
-
 
 	Q_INVOKABLE static void setValue(Key key, const QVariant &value);
 	Q_INVOKABLE static QVariant value(Key key);
@@ -33,9 +35,11 @@ private:
 
 	static QSettings *mSettings;
 	static const QString mVersion;
+	static QHash<Key, QVariant> mDefaultValues;
 
 	static QString stringNameForKey(Key key);
+	static void initDefaultValues();
+	static void checkForMissingDefaultValues();
 };
 
 #endif
-
